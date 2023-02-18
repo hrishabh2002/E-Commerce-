7dad7cbf581666c8643b5d8b874bc3880e0c74c1
hrishabh2002 <91460043+hrishabh2002@users.noreply.github.com>
2023-02-18 22:39:28 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,9 +1,8 @@
-
 import mongoose from "mongoose";
 
-const connection=async(Username,Password)=>{
-const URL=`mongodb://${Username}:${Password}@ac-xdxijhm-shard-00-00.s0fx2ft.mongodb.net:27017,ac-xdxijhm-shard-00-01.s0fx2ft.mongodb.net:27017,ac-xdxijhm-shard-00-02.s0fx2ft.mongodb.net:27017/PROJECT1?ssl=true&replicaSet=atlas-cy5mug-shard-0&authSource=admin&retryWrites=true&w=majority`;
-
+const connection=async(Password)=>{
+//const URL=`mongodb://${Username}:${Password}@ac-xdxijhm-shard-00-00.s0fx2ft.mongodb.net:27017,ac-xdxijhm-shard-00-01.s0fx2ft.mongodb.net:27017,ac-xdxijhm-shard-00-02.s0fx2ft.mongodb.net:27017/PROJECT1?ssl=true&replicaSet=atlas-cy5mug-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const URL=`mongodb+srv://hrishabh:${Password}@ecommerce.s0fx2ft.mongodb.net/?retryWrites=true&w=majority`
     try{
         mongoose.set("strictQuery", false);
         await mongoose.connect(URL,{useUnifiedTopology:true});
