93e2c06202b3aa755b57ab2d0858291b3146afa4
hrishabh2002 <91460043+hrishabh2002@users.noreply.github.com>
2023-03-16 01:21:47 +0530

    Update getproducts.js

--- server/controller/getproducts.js
+++ server/controller/getproducts.js
@@ -1,25 +1,26 @@
-
-import { request } from "express";
 import Product from "../model/ProductSchema.js"
 
-export const getProducts=async(request,response)=>{
-    
-    try{
-        const products=await Product.find({});
-        response.status(200).json(products);
-    }catch(error){
-        response.status(500).json({message:error.message});
-    }
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 
-export const getProductById=async(request,response)=>{
-    try{
-        const id=request.params.id;
-        const product=await Product.findOne({'id':id});
-        response.status(200).json(product);
-    }catch(error){
-        response.status(500).json({message:error.message});
+export const getProductById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await Product.findOne({ _id: id });
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).json({ message: `Product with ID ${id} not found` });
     }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
 // export const getProductById=async(request,response)=>{
 //     console.log(request.params.id);
